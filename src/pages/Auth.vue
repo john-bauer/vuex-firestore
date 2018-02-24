64329d79070fb835237aa0648adc8926dd9ea797
Marco de Vries <skanga@mac256.local>
2018-02-24 14:32:00 +0100

    Minor Changes

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -2,40 +2,77 @@
 
   <div class="home">
 
-    <h1 v-if="userIsAuthenticated">Logout</h1>
+    <b-card no-body>
+      <b-tabs card>
+        <b-tab v-if="!userIsAuthenticated" title="Login">
+          <b-form-group id="loginEmailGroup"
+                        label="Email address:"
+                        label-for="loginEmail">
+            <b-form-input id="exampleInput1"
+                          type="email"
+                          v-model="email"
+                          required
+                          placeholder="Enter email">
+            </b-form-input>
+          </b-form-group>
 
-    <b-form v-if="!userIsAuthenticated">
+          <b-form-group id="loginPasswordGroup"
+                        label="Your Password:"
+                        label-for="loginPassword">
+            <b-form-input id="loginPassword"
+                          type="password"
+                          v-model="password"
+                          required
+                          placeholder="Enter password">
+            </b-form-input>
+          </b-form-group>
+          <b-button @click="onSignin" variant="primary">Login</b-button>
+        </b-tab>
 
-      <h1>Login</h1>
+        <b-tab v-if="!userIsAuthenticated" title="Register">
+          <b-form-group id="registerEmailGroup"
+                        label="Email address:"
+                        label-for="registerEmail">
+            <b-form-input id="registerEmail"
+                          type="email"
+                          v-model="newEmail"
+                          required
+                          placeholder="Enter email">
+            </b-form-input>
+          </b-form-group>
 
-      <b-form-group id="exampleInputGroup1"
-                    label="Email address:"
-                    label-for="exampleInput1">
-        <b-form-input id="exampleInput1"
-                      type="email"
-                      v-model="email"
-                      required
-                      placeholder="Enter email">
-        </b-form-input>
-      </b-form-group>
+          <b-form-group id="registerPasswordGroup"
+                        label="Your Password:"
+                        label-for="registerPassword">
+            <b-form-input id="registerPassword"
+                          type="password"
+                          v-model="newPassword"
+                          required
+                          placeholder="Enter password">
+            </b-form-input>
+          </b-form-group>
 
-      <b-form-group id="exampleInputGroup2"
-                    label="Your Name:"
-                    label-for="exampleInput2">
-        <b-form-input id="exampleInput2"
-                      type="password"
-                      v-model="password"
-                      required
-                      placeholder="Enter password">
-        </b-form-input>
-      </b-form-group>
-
-    </b-form>
+                    <b-form-group id="registerRepeatPasswordGroup"
+                        label="Repeat Password:"
+                        label-for="registerRepeatPassword">
+            <b-form-input id="registerRepeatPassword"
+                          type="password"
+                          v-model="repeatPassword"
+                          required
+                          placeholder="Repeat password">
+            </b-form-input>
+          </b-form-group>
 
 
-    <b-button v-if="!userIsAuthenticated" @click="onSignin" variant="primary">Login</b-button>
-    <b-button v-if="userIsAuthenticated" @click="onLogout" variant="primary">Logout</b-button>
+          <b-button @click="onSignUserUp" variant="primary">Register</b-button>
+        </b-tab>
 
+        <b-tab v-if="userIsAuthenticated" title="Logout">
+          <b-button v-if="userIsAuthenticated" @click="onLogout" variant="primary">Logout</b-button>
+        </b-tab>
+
+      </b-tabs>
+    </b-card>
   </div>
 
 </template>
@@ -46,7 +83,9 @@
       return {
         email: '',
         password: '',
-        passwordCheck: ''
+        newEmail: '',
+        newPassword: '',
+        repeatPassword: ''
       }
     },
     props: {
@@ -77,6 +116,10 @@
       }
     },
     methods: {
+      onSignUserUp () {
+        alert('Registration is currently disabled')
+        // this.$store.dispatch('signUserUp', {email: this.newEmail, password: this.newPassword})
+      },
       onSignin () {
         this.$store.dispatch('signUserIn', {email: this.email, password: this.password})
       },
